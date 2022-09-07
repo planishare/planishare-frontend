9bec9a9d190840d0ff07381a67a4209ca16ef486
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2022-09-06 22:11:21 -0300

    fix(main.ts): replace console.log

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,10 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
     enableProdMode();
+    if (window) {
+        window.console.log = () => {};
+        window.console.warn = () => {};
+    }
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
