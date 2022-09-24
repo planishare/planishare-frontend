e3811112fda0313fa7632e5dde22597cc8de8c4f
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2022-09-24 02:40:59 -0300

    fix(homepage): remove old logic and fix styles

--- src/app/features/homepage/homepage/homepage.component.ts
+++ src/app/features/homepage/homepage/homepage.component.ts
@@ -1,24 +1,10 @@
-import { Component, OnDestroy } from '@angular/core';
-import { isMobile } from 'src/app/shared/utils/window-width';
-import { NavbarService } from 'src/app/shared/services/navbar.service';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-homepage',
     templateUrl: './homepage.component.html',
     styleUrls: ['./homepage.component.scss']
 })
-export class HomepageComponent implements OnDestroy {
-    public isMobile = isMobile;
-
-    constructor(
-        private navbarService: NavbarService
-    ) {
-        this.navbarService.setButtonConfig({
-            backgroundColor: 'var(--pink-light)'
-        });
-    }
-
-    public ngOnDestroy(): void {
-        this.navbarService.resetConfig();
-    }
+export class HomepageComponent {
+    constructor() {}
 }
