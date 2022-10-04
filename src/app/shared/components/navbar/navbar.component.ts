94c1cf4659396b29d156a47e8111f92a61c76f62
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2022-10-03 23:59:47 -0300

    feat(navbar): change background when scroll

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -4,6 +4,7 @@ import { takeUntil } from 'rxjs';
 import { AuthService } from 'src/app/core/services/auth.service';
 import { SidenavService } from 'src/app/core/services/sidenav.service';
 import { WindowResizeService } from '../../services/window-resize.service';
+import { WindowScrollService } from '../../services/window-scroll.service';
 
 import { SidenavComponent } from 'src/app/shared/enums/sidenav.enum';
 
@@ -17,24 +18,24 @@ import { Unsubscriber } from '../../utils/unsubscriber';
 export class NavbarComponent extends Unsubscriber implements OnInit {
     public isUserAuth = false;
     public isDesktop = false;
+    public onScroll = false;
 
     constructor(
         private sidenav: SidenavService,
         private authService: AuthService,
-        private windowResize: WindowResizeService
+        private windowResize: WindowResizeService,
+        private windowScroll: WindowScrollService
     ) {
         super();
-        this.windowResize.isDesktop$
-            .pipe(takeUntil(this.ngUnsubscribe$))
+        this.windowResize.isDesktop$.pipe(takeUntil(this.ngUnsubscribe$))
             .subscribe(value => this.isDesktop = value);
     }
 
     public ngOnInit(): void {
-        this.authService.isAuth$
-            .pipe(takeUntil(this.ngUnsubscribe$))
-            .subscribe(isAuth => {
-                this.isUserAuth = !!isAuth;
-            });
+        this.authService.isAuth$.pipe(takeUntil(this.ngUnsubscribe$))
+            .subscribe(isAuth => this.isUserAuth = !!isAuth);
+        this.windowScroll.scrollYPos$.pipe(takeUntil(this.ngUnsubscribe$))
+            .subscribe(value => this.onScroll = value > 0);
     }
 
     public openSidenav(): void {
