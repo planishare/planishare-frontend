d74777050c37dfdba16ff560165902a0b812f3e5
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2023-06-07 11:43:38 -0400

    fix(edit-profile): refactor logic and rename variables

--- src/app/features/user/pages/edit-profile/edit-profile.component.ts
+++ src/app/features/user/pages/edit-profile/edit-profile.component.ts
@@ -4,6 +4,7 @@ import { MatSnackBar } from '@angular/material/snack-bar';
 import {
     catchError,
     debounceTime,
+    filter,
     forkJoin,
     map,
     Observable,
@@ -11,7 +12,7 @@ import {
     skip,
     startWith,
     takeUntil,
-    tap
+    switchMap
 } from 'rxjs';
 import { Unsubscriber } from 'src/app/shared/utils/unsubscriber';
 
@@ -35,35 +36,32 @@ import { inOutLeftAnimation, inOutRightAnimation } from 'src/app/shared/animatio
     animations: [inOutRightAnimation, inOutLeftAnimation]
 })
 export class EditProfileComponent extends Unsubscriber implements OnInit {
-    public userDetail?: IUserDetail;
-    public isUserDataLoading = true;
+    public loadingUser = true;
+    public loadingInstitutions = false;
+    public loadingLocations = true;
+    public userVerified = false;
+    public saving = false;
+    public saved = true;
 
     public form = new FormGroup({
         email: new FormControl<string>(''),
         firstName: new FormControl<string>('', Validators.minLength(2)),
         lastName: new FormControl<string>('', Validators.minLength(2)),
-        education: new FormControl<number | undefined>(undefined),
-        institution: new FormControl<number | undefined>(undefined),
-        commune: new FormControl<number | undefined>(undefined)
+        education: new FormControl<number|undefined>(undefined),
+        institution: new FormControl<number|undefined>(undefined),
+        commune: new FormControl<number|undefined>(undefined)
     });
 
-    public isVerificated = false;
-    public isLoading = false;
-    public alreadySaved = true;
-
+    public userDetail: IUserDetail|null = null;
     public educations: IEducation[] = [];
     public institutions: IInstitution[] = [];
-    public regionsWithComunnes?: IRegionWithCommunes[];
+    public regionsWithComunnes: IRegionWithCommunes[] = [];
 
-    public isInstitutionsLoading = false;
-    public firstInstitutionReq = true;
-    public isLocationsLoading = true;
     public searchInstitution = new FormControl();
     public searchLocation = new FormControl();
-    public filteredInstitutions?: Observable<IInstitution[]>;
     public filteredLocations?: Observable<IRegionWithCommunes[]>;
 
-    public changePasswordMsg = 'Te enviaremos un correo al email asociado a tu cuenta para que puedas restablecer tu contraseña 📫';
+    public changePasswordMsg = 'Te enviaremos un correo a tu email para que puedas restablecer tu contraseña 📫';
 
     constructor(
         private authService: AuthService,
@@ -79,61 +77,88 @@ export class EditProfileComponent extends Unsubscriber implements OnInit {
         this.form.valueChanges.pipe(
             debounceTime(300),
             skip(1)
-        ).subscribe(() => this.alreadySaved = false);
+        ).subscribe(() => this.saved = false);
     }
 
     public ngOnInit(): void {
-        this.isVerificated = this.authService.user?.firebaseUser.emailVerified ?? false;
+        this.userVerified = this.authService.user?.firebaseUser.emailVerified ?? false;
 
-        forkJoin([this.getEducations(), this.getRegionsWithCommunes(), this.getUserProfile()])
-            .pipe(
+        const dataRequests = forkJoin([
+            this.occupationService.getEducations(),
+            this.locationService.getRegionsWithCommunes(),
+            this.authService.refreshUserDetail()
+        ]);
+
+        dataRequests.pipe(
+            catchError(() => {
+                this.commonSnackbarMsg.showErrorMessage();
+                return of();
+            }),
+            takeUntil(this.ngUnsubscribe$)
+        ).subscribe(([educations, regionsWithComunnes, userDetail]) => {
+            // Set educations
+            this.educations = educations as IEducation[];
+
+            // Set regions with communes
+            this.regionsWithComunnes = regionsWithComunnes as IRegionWithCommunes[];
+            this.loadingLocations = false;
+            this.filteredLocations = this.searchLocation.valueChanges.pipe(
+                startWith(''),
+                map(value => this.locationsFilter(value, this.regionsWithComunnes))
+            );
+
+            // Set user detail
+            this.userDetail = userDetail as IUserDetail;
+            this.changePasswordMsg = `Te enviaremos un correo a ${this.userDetail?.email} para que puedas restablecer tu contraseña 📫`;
+            this.form.patchValue({
+                email: this.userDetail?.email,
+                firstName: this.userDetail?.first_name,
+                lastName: this.userDetail?.last_name,
+                education: this.userDetail?.education?.id,
+                institution: this.userDetail?.institution?.id,
+                commune: this.userDetail?.commune?.id
+            });
+
+            // Set institutions on search
+            if (this.userDetail?.institution) {
+                this.institutions = [this.userDetail.institution];
+            }
+            this.searchInstitution.valueChanges.pipe(
+                filter(value => value),
+                debounceTime(300),
+                switchMap(value => {
+                    this.loadingInstitutions = true;
+                    return this.occupationService.getInstitutions(value);
+                }),
                 catchError(() => {
                     this.commonSnackbarMsg.showErrorMessage();
                     return of();
                 }),
                 takeUntil(this.ngUnsubscribe$)
-            )
-            .subscribe(() => {
-                this.form.patchValue({
-                    email: this.userDetail?.email,
-                    firstName: this.userDetail?.first_name,
-                    lastName: this.userDetail?.last_name,
-                    education: this.userDetail?.education?.id,
-                    institution: this.userDetail?.institution?.id,
-                    commune: this.userDetail?.commune?.id
-                });
-
-                // Filter institutions on input change
-                this.searchInstitution.valueChanges.pipe(
-                    startWith(''),
-                    takeUntil(this.ngUnsubscribe$)
-                ).subscribe(value => {
-                    if (!!this.userDetail?.institution && this.firstInstitutionReq) {
-                        this.firstInstitutionReq = false;
-                        this.filteredInstitutions = of([this.userDetail.institution]);
-                    } else if (!!value) {
-                        this.filteredInstitutions = this.getInstitutions(value);
-                    }
-                });
-
-                this.isUserDataLoading = false;
-                this.alreadySaved = true;
+            ).subscribe(institutions => {
+                this.institutions = institutions.results;
+                this.loadingInstitutions = false;
             });
+
+            // Change status
+            this.loadingUser = false;
+            this.saved = true;
+        });
     }
 
     public save(event: Event): any {
         event.preventDefault();
 
-        if (this.form.invalid || this.isLoading) {
+        if (this.form.invalid || this.saving) {
             return;
         }
 
-        if (this.alreadySaved) {
+        if (this.saved) {
             this.matSnackBar.open('Perfil actualizado 🙌', 'Cerrar', { duration: 2000 });
             return;
         }
 
-        this.isLoading = true;
+        this.saving = true;
         const userData: IUserForm = {
             first_name: this.form.controls.firstName.value ?? undefined,
             last_name: this.form.controls.lastName.value ?? undefined,
@@ -145,15 +170,15 @@ export class EditProfileComponent extends Unsubscriber implements OnInit {
             .pipe(
                 catchError(() => {
                     this.commonSnackbarMsg.showErrorMessage();
-                    this.isLoading = false;
+                    this.saving = false;
                     return of();
                 }),
                 takeUntil(this.ngUnsubscribe$)
             )
             .subscribe(newUserDetail => {
                 this.matSnackBar.open('Perfil actualizado 🙌', 'Cerrar', { duration: 2000 });
-                this.isLoading = false;
-                this.alreadySaved = true;
+                this.saving = false;
+                this.saved = true;
 
                 const userDetail = this.authService.getUserDetail();
                 userDetail?.update(newUserDetail);
@@ -161,76 +186,31 @@ export class EditProfileComponent extends Unsubscriber implements OnInit {
             });
     }
 
-    public getEducations(): Observable<IEducation[]> {
-        return this.occupationService.getEducations().pipe(
-            tap(resp => this.educations = resp)
-        );
-    }
-
-    public getInstitutions(search: string): Observable<IInstitution[]> {
-        this.isInstitutionsLoading = true;
-        return this.occupationService.getInstitutions(search).pipe(
-            map(resp => {
-                this.institutions = resp.results;
-                this.isInstitutionsLoading = false;
-                return resp.results;
-            }),
-            catchError(() => {
-                this.commonSnackbarMsg.showErrorMessage();
-                return of();
-            }),
-            takeUntil(this.ngUnsubscribe$)
-        );
-    }
-
-    public getRegionsWithCommunes(): Observable<IRegionWithCommunes[]> {
-        return this.locationService.getRegionsWithCommunes().pipe(
-            tap(resp => {
-                this.regionsWithComunnes = resp;
-                this.isLocationsLoading = false;
-                this.filteredLocations = this.searchLocation.valueChanges.pipe(
-                    startWith(''),
-                    map(value => this.locationsFilter(value, resp))
-                );
-            })
-        );
-    }
-
-    public getUserProfile(): Observable<IUserDetail | undefined> {
-        return this.authService.refreshUserDetail().pipe(
-            tap(resp => {
-                this.userDetail = resp;
-                this.changePasswordMsg = `Te enviaremos un correo a ${resp?.email} para que puedas restablecer tu contraseña 📫`;
-            })
-        );
-    }
-
     private locationsFilter(
         searchValue: string,
         optionList: IRegionWithCommunes[]): IRegionWithCommunes[] {
-        if (!!searchValue) {
-            searchValue = searchValue.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-
-            const results: IRegionWithCommunes[] = [];
-            optionList.forEach(region => {
-                const communesFiltered: ICommune[] = [];
-                region.communes.forEach(commune => {
-                    const communeNameNormalized = commune.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-                    if (communeNameNormalized.includes(searchValue)) {
-                        communesFiltered.push(commune);
-                    }
-                });
-                if (communesFiltered.length) {
-                    results.push({
-                        ...region,
-                        communes: communesFiltered
-                    });
-                }
-            });
-            return results;
-        } else {
+        if (!searchValue) {
             return optionList;
         }
+
+        const results: IRegionWithCommunes[] = [];
+        searchValue = searchValue.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+        optionList.forEach(region => {
+            const communesFiltered: ICommune[] = [];
+            region.communes.forEach(commune => {
+                const communeNameNormalized = commune.name.toLowerCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+                if (communeNameNormalized.includes(searchValue)) {
+                    communesFiltered.push(commune);
+                }
+            });
+            if (communesFiltered.length) {
+                results.push({
+                    ...region,
+                    communes: communesFiltered
+                });
+            }
+        });
+        return results;
     }
 
     public resendEmail(): void {
