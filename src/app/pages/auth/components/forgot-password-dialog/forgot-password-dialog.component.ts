619f3fea4d969417e31ce1802e1236e0290d1cd8
Nicolás Bustamante Estay <nicosmic8@gmail.com>
2023-08-28 19:23:36 -0400

    feat(fotgot-password-dialog): Add new styles

--- src/app/pages/auth/components/forgot-password-dialog/forgot-password-dialog.component.ts
+++ src/app/pages/auth/components/forgot-password-dialog/forgot-password-dialog.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { take } from 'rxjs';
 import { FirebaseAuthService } from 'src/app/core/services/firebase-auth.service';
-import { inOutLeftAnimation, inOutRightAnimation } from 'src/app/shared/animations/animations';
 
 @Component({
     selector: 'app-forgot-password-dialog',
     templateUrl: './forgot-password-dialog.component.html',
-    styleUrls: ['./forgot-password-dialog.component.scss'],
-    animations: [inOutLeftAnimation, inOutRightAnimation]
+    styleUrls: ['./forgot-password-dialog.component.scss']
 })
 export class ForgotPasswordDialogComponent {
     public email = new FormControl<string>('', [Validators.required, Validators.email]);
